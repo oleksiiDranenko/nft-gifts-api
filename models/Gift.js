f65abfcaa78214013a4c342bdf709968a3a3ccbf
oleksiiDranenko <alexdranenko2004@icloud.com>
2025-05-24 22:40:17 +0100

    added preSale property for gift model

--- models/Gift.js
+++ models/Gift.js
@@ -11,7 +11,8 @@ const GiftSchema = new mongoose.Schema(
         upgradePrice: {type: Number, required: true},
         initTonPrice: { type: Number, required: true },
         initUsdPrice: { type: Number, required: true },
-        staked: { type: Boolean, required: false }
+        staked: { type: Boolean, required: false },
+        preSale: {type: Boolean, required: false}
     },
     { 
         collection: 'gifts',
